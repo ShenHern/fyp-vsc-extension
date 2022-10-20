cb778bf908c8edd7ac2bd99e0b04b08d54ec43c7
ShenHern <go.shenhern@hotmail.com>
2022-10-20 12:31:13 +0800

    findTrace and addToCache

--- src/functions.ts
+++ src/functions.ts
@@ -21,3 +21,47 @@ export function splitComponents(s: string) {
 
     return [compName, compClazz, compNode];
 }
+
+/**
+ * A function to find the index of the trace a given event belongs to.
+ * @param cache an dictionary that maps the eventID to index in the traces list
+ * @param eventRow an event at a particular row in trace.json
+ * @returns the index of the trace the event belongs to; otherwise undefined
+ */
+export function findTrace(cache: { [id: string]: number; }, eventRow: { [x: string]: any; }) {
+    if ("threadID" in eventRow) {
+        let id: string = eventRow["threadID"];
+        if (id in cache) {
+            return cache[id];
+        }
+    }
+    if ("stimulus" in eventRow) {
+        let id: string = eventRow["stimulus"]["messageID"];
+        if (id in cache) {
+            return cache[id];
+        }
+    }
+    return undefined;
+}
+
+/**
+ * A function to cache the index of the trace that a given event belongs to.
+ * @param cache an dictionary that maps the eventID to index in the traces list
+ * @param eventRow an event at a particular row in trace.json
+ * @param traceID the index of the trace that the event belongs to
+ */
+export function addToCache(cache: { [id: string]: number; }, eventRow: { [x: string]: any; }, traceID: number) {
+    if ("threadID" in eventRow) {
+        let id: string = eventRow["threadID"];
+        cache[id] = traceID;
+    }
+    if ("stimulus" in eventRow) {
+        let id: string = eventRow["stimulus"]["messageID"];
+        cache[id] = traceID;
+    }
+    if ("response" in eventRow) {
+        let id: string = eventRow["response"]["messageID"];
+        cache[id] = traceID;
+    }
+}
+
