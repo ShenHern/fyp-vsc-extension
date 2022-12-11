cdc10981085d254deded990573e02d49a81533bf
ShenHern <go.shenhern@hotmail.com>
2022-12-11 15:57:43 +0800

    summary comment for types

--- src/types.ts
+++ src/types.ts
@@ -1,16 +1,21 @@
+/**
+ * A cache to store the whole tree of events when forming the tree.
+ * The cache maps a message ID to a tree node.
+ * @param msgId: the message ID as a string
+ */
 export interface TreeCache {
     [msgId: string]: TreeNode;
 }
 
 /**
  * TreeNode consists of
- * id: the id of a particular event
- * parent: the parent that caused this event
- * children: the children that were spawned from this event
- * time: the time this event took place
- * clazz: the class of the event
- * sender: the sender node for this event
- * recipient: the recipient node for this event
+ * @param id: the id of a particular event
+ * @param parent: the parent that caused this event
+ * @param children: the children that were spawned from this event
+ * @param time: the time this event took place
+ * @param clazz: the class of the event
+ * @param sender: the sender node for this event
+ * @param recipient: the recipient node for this event
  */
 export interface TreeNode {
     id: string;
