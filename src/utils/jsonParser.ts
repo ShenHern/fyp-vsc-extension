a7381f50805aa2a95f68815b3926af6f4eba0f8c
Kristine Lee <64589735+kristpykreme@users.noreply.github.com>
2023-02-23 14:27:15 +0800

    Clean up

--- src/utils/jsonParser.ts
+++ src/utils/jsonParser.ts
@@ -9,18 +9,6 @@ export const parser = (jsonStr: string) => {
   for (let i = 0; i < json.events.length; i++) {
     groupMap.set(i, json.events[i]);
   }
-  // for (let i = 0; i < json.events.length; i++) {
-  //   const traces = analyse(json.events[i].events);
-  //   const sequenceResult = sequence(traces[3][2]);
-  //   if (sequenceResult === undefined) {
-  //     throw new Error("Could not find sequence in trace.");
-  //   }
-  //   // console.log(sequenceResult[2]); //root of tree after sequencing a trace
-  //   const output = sortTreeByTime(sequenceResult[2])
-
-  //   // return format(output);
-  //   return output;
-// }
   return groupMap;
 };
 
@@ -30,9 +18,7 @@ export const format = (traces: any) => {
   if (sequenceResult === undefined) {
     throw new Error("Could not find sequence in trace.");
   }
-  // console.log(sequenceResult[2]); //root of tree after sequencing a trace
   const output = sortTreeByTime(sequenceResult[2]);
-  // return nodeFormat(output);
   const result : Array<any>[] = [];
   result.push(actors);
   result.push(output);
@@ -52,32 +38,3 @@ export const eventsArray = (jsonStr: string) => {
   }
   return eventArray;
 };
-
-function nodeFormat(output : TreeNode[]) {
-
-  console.log(output);
-  const nodes : NodeData[] = [];
-
-  for(let i = 0; i < Object.keys(output).length; i++) {
-    const currNode =  {
-      id: `${output[i].id}`,
-      text: `${output[i].time}`
-    };
-    nodes.push(currNode);
-  }
-  return nodes;
-}
-
-export function edgeFormat(nodes : NodeData[]) {
-  const edges : EdgeData[] = [];
-
-  for(let i = 0; i < nodes.length-1; i++) {
-    const currEdge = {
-      id: `${nodes[i].id}+${nodes[i+1].id}`,
-      from: `${nodes[i].id}`,
-      to: `${nodes[i+1].id}`
-    };
-    edges.push(currEdge);
-  }
-  return(edges);
-}
